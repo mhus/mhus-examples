094a07eb5c407fc30d9f2e4562e11d59edd610fc
Mike Hummel <mh@mhus.de>
2016-11-17 21:47:22 +0100

    extend sample

--- vaadin-addon-ports/src/main/java/de/mhus/examples/vaadin/addon/ports/FlatSelectTab.java
+++ vaadin-addon-ports/src/main/java/de/mhus/examples/vaadin/addon/ports/FlatSelectTab.java
@@ -6,27 +6,74 @@ import org.vaadin.miki.flatselect.FlatSelect;
 
 import com.vaadin.ui.Alignment;
 import com.vaadin.ui.Component;
+import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.Notification;
 import com.vaadin.ui.Panel;
 import com.vaadin.ui.VerticalLayout;
 
-public class FlatSelectTab extends Panel implements Component {
+public class FlatSelectTab extends VerticalLayout implements Component {
 
 	public FlatSelectTab() {
         // Initialize our new UI component
 		setSizeFull();
-        final FlatSelect component = new FlatSelect("Pick one", Arrays.asList("options", "available", "in", "flat", "select", "are", "plenty"));
-        component.setOptionsPerRow(1);
-      //  component.setOptionWidth("100%");
-
-        component.addValueChangeListener(e -> component.setCaption(component.getItemCaption(e.getProperty().getValue())));
-
-        // Show it in the middle of the screen
-        final VerticalLayout layout = new VerticalLayout();
-        layout.setStyleName("demoContentLayout");
-        layout.setSizeFull();
-        layout.addComponent(component);
-        layout.setComponentAlignment(component, Alignment.MIDDLE_CENTER);
-        setContent(layout);
-
+//		addStyleName("flatselect");
+		{
+			HorizontalLayout container = new HorizontalLayout();
+	        final FlatSelect component = new FlatSelect("Pick one", Arrays.asList("options", "available", "in", "flat", "select", "are", "plenty"));
+	        container.addStyleName("flatlabel");
+	        container.addStyleName("flatleft");
+	        container.addStyleName("flatselectable");
+	        component.setOptionsPerRow(2);
+	        component.setOptionsWidth("200px");
+	
+	        component.addValueChangeListener(e -> component.setCaption(component.getItemCaption(e.getProperty().getValue())));
+	
+	        // Show it in the middle of the screen
+	        final VerticalLayout layout = new VerticalLayout();
+	        layout.setStyleName("demoContentLayout");
+	        layout.setSizeFull();
+	        layout.addComponent(component);
+	        layout.setComponentAlignment(component, Alignment.MIDDLE_CENTER);
+	        container.addComponent(layout);
+	        addComponent(container);
+		}
+		{
+			HorizontalLayout container = new HorizontalLayout();
+	        final FlatSelect component = new FlatSelect("Pick one", Arrays.asList("options", "available", "in", "flat", "select", "are", "plenty"));
+	        container.addStyleName("flatselectable");
+	        component.setOptionsPerRow(2);
+	        component.setOptionsWidth("200px");
+	
+	        component.addValueChangeListener(e -> component.setCaption(component.getItemCaption(e.getProperty().getValue())));
+	
+	        // Show it in the middle of the screen
+	        final VerticalLayout layout = new VerticalLayout();
+	        layout.setStyleName("demoContentLayout");
+	        layout.setSizeFull();
+	        layout.addComponent(component);
+	        layout.setComponentAlignment(component, Alignment.MIDDLE_CENTER);
+	        container.addComponent(layout);
+	        addComponent(container);
+		}
+		{
+			HorizontalLayout container = new HorizontalLayout();
+	        final FlatSelect component = new FlatSelect("Pick one", Arrays.asList("options", "available", "in", "flat", "select", "are", "plenty"));
+	        container.addStyleName("flatselect");
+	        container.addStyleName("flatleft");
+	        container.addStyleName("flatselectable");
+	        component.setOptionsPerRow(2);
+	        component.setOptionsWidth("200px");
+	
+	        component.addValueChangeListener(e -> Notification.show("Click " + e.getProperty().getValue() ) );
+	
+	        // Show it in the middle of the screen
+	        final VerticalLayout layout = new VerticalLayout();
+	        layout.setStyleName("demoContentLayout");
+	        layout.setSizeFull();
+	        layout.addComponent(component);
+	        layout.setComponentAlignment(component, Alignment.MIDDLE_CENTER);
+	        container.addComponent(layout);
+	        addComponent(container);
+		}
 	}
 }
