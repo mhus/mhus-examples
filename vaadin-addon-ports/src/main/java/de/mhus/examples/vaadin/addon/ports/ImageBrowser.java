bc4bb2f65c8119941f463c6e8463eef593faae8d
Mike Hummel <mikehummel@Nofretete.fritz.box>
2017-02-07 21:08:12 +0100

    add ckeditor browser return calls

--- vaadin-addon-ports/src/main/java/de/mhus/examples/vaadin/addon/ports/ImageBrowser.java
+++ vaadin-addon-ports/src/main/java/de/mhus/examples/vaadin/addon/ports/ImageBrowser.java
@@ -1,12 +1,48 @@
 package de.mhus.examples.vaadin.addon.ports;
 
+import com.vaadin.server.Page;
+import com.vaadin.server.ThemeResource;
+import com.vaadin.ui.Button;
 import com.vaadin.ui.Component;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.Button.ClickEvent;
 
 public class ImageBrowser extends VerticalLayout implements Component {
 
+	private String funcNum;
+	private String lang;
+
 	public ImageBrowser() {
+		
+		String fragment = Page.getCurrent().getUriFragment();
+		fragment = fragment.split("\\?")[1];
+		String[] parts = fragment.split("\\&");
+		for (String part : parts) {
+			if (part.startsWith("CKEditorFuncNum="))
+				funcNum = part.substring("CKEditorFuncNum=".length());
+			else
+			if (part.startsWith("langCode="))
+				lang = part.substring("langCode=".length());
+		}
+
 		addComponent(new Label("ImageBrowser"));
+		
+		{
+			Button b = new Button("Image 1");
+			b.addClickListener(new Button.ClickListener() {
+				
+				@Override
+				public void buttonClick(ClickEvent event) {
+					
+					String img = "/VAADIN/themes/vaadinsample/img/table-logo.png";
+					
+					String myCode = "window.opener.CKEDITOR.tools.callFunction("+funcNum+", \""+img+"\");window.close();";
+					Page.getCurrent().getJavaScript().execute(myCode);
+				}
+			});
+			addComponent(b);
+		}
+
 	}
 }
--- vaadin-addon-ports/src/main/java/de/mhus/examples/vaadin/addon/ports/LinkBrowser.java
+++ vaadin-addon-ports/src/main/java/de/mhus/examples/vaadin/addon/ports/LinkBrowser.java
@@ -1,12 +1,43 @@
 package de.mhus.examples.vaadin.addon.ports;
 
+import com.vaadin.server.Page;
+import com.vaadin.ui.Button;
 import com.vaadin.ui.Component;
 import com.vaadin.ui.Label;
+import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.Button.ClickEvent;
 
 public class LinkBrowser extends VerticalLayout implements Component {
 
+	private Object funcNum;
+	private Object lang;
+
 	public LinkBrowser() {
+		
+		String fragment = Page.getCurrent().getUriFragment();
+		fragment = fragment.split("\\?")[1];
+		String[] parts = fragment.split("\\&");
+		for (String part : parts) {
+			if (part.startsWith("CKEditorFuncNum="))
+				funcNum = part.substring("CKEditorFuncNum=".length());
+			else
+			if (part.startsWith("langCode="))
+				lang = part.substring("langCode=".length());
+		}
+		
 		addComponent(new Label("LinkBrowser"));
+		{
+			Button b = new Button("Link 1");
+			b.addClickListener(new Button.ClickListener() {
+				
+				@Override
+				public void buttonClick(ClickEvent event) {
+					String myCode = "window.opener.CKEDITOR.tools.callFunction("+funcNum+", \"/nav/page.html\");window.close();";
+					Page.getCurrent().getJavaScript().execute(myCode);
+				}
+			});
+			addComponent(b);
+		}
 	}
 }
